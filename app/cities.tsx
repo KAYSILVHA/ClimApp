245e97feb627bd4faf3bd5a2d592179d1b15d8e0
KAYSILVHA <kaysilvha@gmail.com>
2025-03-26 18:00:23 -0300

    ajustando tela

--- app/cities.tsx
+++ app/cities.tsx
@@ -1,9 +1,98 @@
-import { View } from "react-native"
+import { StyleSheet, View, Text, Image, ScrollView, TextInput } from "react-native"
+import { LinearGradient } from 'expo-linear-gradient';
+import citiesData from "../data/cities.json";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useEffect, useState } from "react";
 
 const Cities = () => {
+    const [search, setSearch] = useState("");
+    const [filteredCities, setFilteredCities] = useState(citiesData);
+
+    useEffect(() => {
+        const newFilteredCities = citiesData.filter(city => city.city.includes(search));
+        setFilteredCities(newFilteredCities);
+
+    }, [search]);
+
     return (
-        <View></View>
-    )
-};
+        <LinearGradient
+            colors={["#00457D", "#05051F"]}
+            style={style.container}
+        >
+            <View style={style.inputContainer}>
+                <TextInput
+                    placeholder="Digite a cidade"
+                    style={style.input}
+                    value={search}
+                    onChangeText={(value) => setSearch(value)}
+                />
+                <MaterialIcons name='search' size={18} color={"#fff"} />
+            </View>
+            <ScrollView>
+                <View style={style.scrollList}>
+                    {citiesData.map(city => (
+                        <View style={style.listItem} key={city.city}>
+                            <Image style={style.cityImage} source={require("../assets/images/Vector.png")} />
+                            <Text style={style.cityName}>{city.city.replace(", ", " - ")}</Text>
+                            <Text style={style.cityTemp}>{city.temp}º</Text>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
+
+        </LinearGradient>
+    );
+}
 
 export default Cities;
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 15,
+        gap: 16,
+        paddingTop: 40,
+    },
+    scrollList: {
+        gap: 16,
+    },
+    listItem: {
+        height: 63,
+        width: "100%",
+        backgroundColor: "rgba(255,255,255, 0.15)",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderRadius: 16,
+        flexDirection: "row",
+        paddingHorizontal: 16,
+    },
+    cityName: {
+        fontSize: 16,
+        color: "#fff",
+        fontFamily: "Montserrat_500Medium"
+    },
+    cityTemp: {
+        color: "#fff",
+        fontSize: 25,
+        fontFamily: "Montserrat_700Bold",
+    },
+    cityImage: {
+        width: 27,
+        height: 24,
+    },
+    inputContainer: {
+        height: 36,
+        width: "100%",
+        backgroundColor: "rgba(255, 255, 255, 0.15)",
+        borderRadius: 24,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 16,
+    },
+    input: {
+        color: "#fff",
+        fontSize: 16,
+        fontFamily: "Montserrat_500Medium",
+    }
+})
